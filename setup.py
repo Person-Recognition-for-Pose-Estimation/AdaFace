de68c476fcf269306cd1d79a6419beaa2873483d
William Parker <wiparker2020@gmail.com>
2025-01-18 00:44:36 -0500

    Edits setup.py

--- setup.py
+++ setup.py
@@ -10,7 +10,12 @@ setup(
     packages=find_packages(),
     install_requires=requirements,
     include_package_data=True,
+
+    package_data={
+        '': ['*.py'],
+    },
     
-    # Choose your license
+    py_modules=['convert', 'inference', 'net', 'train_val', 'config', 'data', 'evaluate_utils', 'head', 'utils'],
+    
     license='MIT',
 )
