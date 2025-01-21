3aa32bd69572d4ab1205298e78d057516fc885fb
William Parker <wiparker2020@gmail.com>
2025-01-21 11:26:31 -0800

    Moves everything inside a folder to follow Python packaging best practices.

--- setup.py
+++ setup.py
@@ -7,11 +7,9 @@ with open('requirements.txt') as f:
 setup(
     name="ada_face",
     version="0.0.1",
-    packages=find_packages() + ['.'],
+    packages=find_packages(),
     install_requires=requirements,
     include_package_data=True,
-    
-    py_modules=['convert', 'inference', 'net', 'train_val', 'config', 'data', 'evaluate_utils', 'head', 'utils'],
     
     license='MIT',
 )
